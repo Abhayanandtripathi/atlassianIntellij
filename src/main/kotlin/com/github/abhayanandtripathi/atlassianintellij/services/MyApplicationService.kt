abaa74c522c432992bef54650116b6ddb893b713
GitHub Action <action@github.com>
2021-01-10 12:42:52 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/abhayanandtripathi/atlassianintellij/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.abhayanandtripathi.atlassianintellij.services
+
+import com.github.abhayanandtripathi.atlassianintellij.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
